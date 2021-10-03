d07ae7eb85f5bd45ca51951729e724a2c34473b9
joseMix <js.silva.010@gmail.com>
2021-10-02 21:46:26 -0300

    implementacion de metodos API bonita

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const Bonita = require("../model/bonita.js");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+//Ruta para comprobar metodos de la API mediante metodo TestALL
+router.get("/testAPI", function (req, res, next) {
+  Bonita.testAll();
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
